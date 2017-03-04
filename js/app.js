868076b03ecfc044a6a83029bfeafea7689a29fc
James Liu <disposedtrolley@gmail.com>
2017-03-05 09:49:51 +1100

    removed jquery. added map markers

--- js/app.js
+++ js/app.js
@@ -1,11 +1,45 @@
+var restaurants = [
+    {
+        name: 'Do 1 Thing',
+        lat: -37.85773,
+        lng: 145.0293009
+    },
+    {
+        name: 'BOY & Co.',
+        lat: -37.8586496,
+        lng: 145.028872
+    },
+    {
+        name: 'Millstone',
+        lat: -37.865762,
+        lng: 145.0295536
+    }
+];
+
+var Marker = function(data) {
+    this.name = ko.observable(data.name);
+    this.lat = ko.observable(data.lat);
+    this.lng = ko.observable(data.lng);
+};
+
 function initMap() {
-    var melbourne = {lat: -37.8136, lng: 144.9631};
+    var malvern = {lat: -37.8584378, lng: 145.0181495};
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 15,
-      center: melbourne
+      center: malvern
     });
-    var marker = new google.maps.Marker({
-      position: melbourne,
-      map: map
+    restaurants.forEach(function(restaurant) {
+        var marker = new google.maps.Marker({
+            position: {lat: restaurant.lat, lng: restaurant.lng},
+            map: map
+        });
     });
 }
+
+
+var ViewModel = function() {
+    var self = this;
+    initMap();
+};
+
+ko.applyBindings(new ViewModel());
