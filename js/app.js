ef7bda45dd24c2786c286a687c008c7062bb0b63
James Liu <disposedtrolley@gmail.com>
2017-03-05 15:34:07 +1100

    reorganised functions

--- js/app.js
+++ js/app.js
@@ -1,29 +1,63 @@
-function initMap() {
-    var malvern = {lat: -37.8609852, lng: 145.0268996};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 16,
-      center: malvern
-    });
-    restaurants.forEach(function(restaurant) {
-        var marker = new google.maps.Marker({
-            position: {lat: restaurant.lat, lng: restaurant.lng},
-            map: map
-        });
-    });
-}
+var map;
 
 var Restaurant = function(data) {
+    var marker;
     this.name = ko.observable(data.name);
     this.address = ko.observable(data.address);
     this.lat = ko.observable(data.lat);
     this.lng = ko.observable(data.lng);
+    marker = new google.maps.Marker({
+        position: {lat: data.lat, lng: data.lng},
+        map: map
+    });
+
+    this.marker = ko.observable(marker);
 };
 
 var ViewModel = function() {
     var self = this;
-    this.restaurantList = ko.observableArray([]);
-    restaurants.forEach(function(restaurant) {
-        self.restaurantList.push(new Restaurant(restaurant));
+
+    self.restaurantList = ko.observableArray([]);
+    self.currentRestaurant = ko.observable();
+
+    self.initMap = function() {
+        var malvern = {lat: -37.8609852, lng: 145.0268996};
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: malvern
+        });
+    };
+
+    self.addRestaurants = function() {
+        restaurants.forEach(function(restaurant) {
+            self.restaurantList.push(new Restaurant(restaurant));
+        });
+        self.currentRestaurant = ko.observable(self.restaurantList()[0]);
+    };
+
+    self.addClickHandlers = function() {
+        self.restaurantList().forEach(function(restaurant) {
+            restaurant.marker().addListener('click', function() {
+                self.setRestaurant(restaurant);
+                console.log(self.currentRestaurant().name());
+                self.openModal();
+            });
+        });
+    };
+
+    self.setRestaurant = function(clickedRestaurant) {
+        self.currentRestaurant(clickedRestaurant);
+    };
+
+    self.openModal = function() {
+        console.log(self.currentRestaurant());
+        $('#detail-modal').modal('show');
+    };
+
+    google.maps.event.addDomListener(window, 'load', function() {
+        self.initMap();
+        self.addRestaurants();
+        self.addClickHandlers();
     });
 };
 
